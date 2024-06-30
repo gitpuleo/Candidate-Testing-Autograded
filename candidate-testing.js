2859bbe5d703e0763e59102924dc53fbefc7cb3d
nfp <154305665+gitpuleo@users.noreply.github.com>
2024-06-30 17:44:56 -0500

    Code now passes all 17 tests and operates as required

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,7 +12,7 @@ let candidateAnswer = ""
 
 
 //TODO: Variables for Part 2
-let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometers == 5000 meters? ", "(5+3)/2*10=? ", 
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", 
 "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "
 ];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
@@ -43,12 +43,26 @@ function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
   
   questions.forEach((question, i) => {
-    console.log(`Question 1 cadidate answer: ${candidateAnswers[i]}. \nCorrect answer: ${correctAnswers[i]}`);
+    console.log(`Candidate answer: ${candidateAnswers[i]}. \nCorrect answer: ${correctAnswers[i]}`);
 })
 
 
 
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
+  let grade = 0; //TODO 3.2 use this variable to calculate the candidates score.
+ for (i = 0; i < 5; i++) {
+    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
+        grade += 20;
+    }
+ } console.log(grade);
+
+ if (grade >= 80) {
+  console.log(`Congratulations ${candidateName}, you scored ${grade}%, meaning you pass!`)
+ } else {
+  console.log(`Sorry ${candidateName}, your score of ${grade}% is not sufficient to pass.`)
+ }
+
+
+
 
 
   return grade;
